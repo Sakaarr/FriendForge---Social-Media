e1b973853dc5f5dc551a28d768854d304449628f
Sakar Bhandari <bhandarisakar@outlook.com>
2023-11-12 02:00:46 +0545

    Uploading in Progress

--- friendforge/core/urls.py
+++ friendforge/core/urls.py
@@ -3,4 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index , name='home'),
+    path('signup', views.signup , name='signup'),
+    path('signin', views.signin , name='signin'),
+    path('settings', views.settings , name='settings'),
+    path('logout', views.logout , name="logout"),
+    path('upload', views.upload , name="upload"),
+
 ]
